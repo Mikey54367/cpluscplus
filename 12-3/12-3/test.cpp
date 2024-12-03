a7b603c4cec606ca2cbaa59e0393608f99c65a53
land <727642523@qq.com>
2024-12-03 20:01:30 +0800

    dp*1

---
+++ 12-3/12-3/test.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int numDistinct(string s, string t) {
+    int m = s.size();
+    int n = t.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    for (int i = 0; i <= m; ++i)
+    {
+        dp[i][0] = 1;
+    }
+    for (int i = 1; i <= m; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+        {
+            dp[i][j] = dp[i - 1][j];
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % ((int)pow(10, 9) + 7);
+        }
+    }
+    return dp[m][n];
+}
+int main()
+{
+    numDistinct("babgbag", "bag");
+    
+	return 0;
+}
